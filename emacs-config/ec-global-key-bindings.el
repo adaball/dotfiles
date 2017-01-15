fff9782b71e9936b14af562b9b592111c4d7304b
Adam Ball <etc@adamball.org>
2017-01-15 13:56:01 -0600

    removed cider-repl-clear-buffer keybinding out of global declarations and into the cider repl mode

--- emacs-config/ec-global-key-bindings.el
+++ emacs-config/ec-global-key-bindings.el
@@ -6,7 +6,4 @@
 ;; comment line with Ctrl-Shift-/
 (global-set-key (kbd "C-?") 'toggle-comment-on-line)
 
-;; clear cider-repl buffer
-;(global-set-key (kbd "C-1") 'cider-repl-clear-buffer)
-
 (provide 'ec-global-key-bindings)
